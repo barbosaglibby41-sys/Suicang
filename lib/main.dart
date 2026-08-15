65ba588871e4c6ae284e54c8ec037839f011ec4e
Suicang Developer <suicang-dev@users.noreply.github.com>
2026-08-15 23:39:44 +0800

    fix: provide Riverpod scope for chat screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'app/suicang_app.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(const SuicangApp());
+  runApp(const ProviderScope(child: SuicangApp()));
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,9 +1,10 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:suicang/app/suicang_app.dart';
 
 void main() {
   testWidgets('Suicang opens the discover screen', (tester) async {
-    await tester.pumpWidget(const SuicangApp());
+    await tester.pumpWidget(const ProviderScope(child: SuicangApp()));
     await tester.pumpAndSettle();
 
     expect(find.text('Suicang'), findsOneWidget);
