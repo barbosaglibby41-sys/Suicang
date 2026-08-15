7519635c85e7409543606e118bd7b28722c74c48
Suicang Developer <suicang-dev@users.noreply.github.com>
2026-08-15 23:33:05 +0800

    test: navigate to chat through app shell

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:go_router/go_router.dart';
 import 'package:suicang/app/suicang_app.dart';
 
 void main() {
@@ -12,7 +11,7 @@ void main() {
     expect(find.text('快速开始'), findsOneWidget);
     expect(find.textContaining('让灵感'), findsOneWidget);
 
-    tester.state(find.byType(Navigator)).context.go('/chat');
+    await tester.tap(find.text('聊天'));
     await tester.pumpAndSettle();
     expect(find.text('Luna'), findsOneWidget);
     expect(find.textContaining('写下你的回复'), findsOneWidget);
